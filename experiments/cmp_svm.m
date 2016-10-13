07202c7d51ccf79d6a19af4c8bca631d7504c52a
Kenyon Tsai <cytsai@gmail.com>
2016-10-13 16:11:49 -0400

    corrected svm

--- experiments/cmp_svm.m
+++ experiments/cmp_svm.m
@@ -1,14 +1,14 @@
+function cmp_svm()
+
 diary('cmp_svm.log');
 
-system('python tsnet/datasets.py');
+%system('python tsnet/datasets.py');
 datasets = {'mnist' 'cifar10' 'svhn2'};
+L = 10;
 
-clip   = @(K)max(min(K,1),-1);
-ssrelu = @(K)(sqrt(1-K.^2) + (pi-acos(K)).*K)/pi;
-tsrelu = @(K)(1 - acos(K)/pi).*K;
-
-kern = { ssrelu ,  tsrelu };
-name = {'SSReLU', 'TSReLU'};
+clip   = @(K)      max(min(K,1),-1);
+ssrelu = @(Kss)    (sqrt(1-Kss.^2) + (pi-acos(Kss)).*Kss)/pi;
+tsrelu = @(Kss,Kts)(1 - acos(Kss)/pi).*Kts;
 
 for i = 1:length(datasets)
     
@@ -27,40 +27,73 @@ for i = 1:length(datasets)
     y_val = double(y_val);
     y_tst = double(y_tst);
     
-    for k = [1 2]
-        for c = 10 .^ [0 1 2 3]
-            
-            K_trn = zeros(size(X_trn,1)+1, size(X_trn,1), 'single'); K_trn(1,:) = 1:size(X_trn,1);
-            K_val = zeros(size(X_trn,1)+1, size(X_val,1), 'single'); K_val(1,:) = 1:size(X_val,1);
-            K_tst = zeros(size(X_trn,1)+1, size(X_tst,1), 'single'); K_tst(1,:) = 1:size(X_tst,1);
-            
-            tic;
-            K_trn(2:end,:) = clip(X_trn * X_trn');
-            K_val(2:end,:) = clip(X_trn * X_val'); t_base = toc;
-            K_tst(2:end,:) = clip(X_trn * X_tst');
-            
-            acc = 0;
-            for l = 0:100
+    for c = [0 1 2 3]
+        
+        Kss_trn = zeros(size(X_trn,1)+1, size(X_trn,1), 'single'); Kss_trn(1,:) = 1:size(X_trn,1);
+        Kss_val = zeros(size(X_trn,1)+1, size(X_val,1), 'single'); Kss_val(1,:) = 1:size(X_val,1);
+        Kss_tst = zeros(size(X_trn,1)+1, size(X_tst,1), 'single'); Kss_tst(1,:) = 1:size(X_tst,1);
+        
+        Rss = zeros(L+1,3);
+        
+        tic;
+        Kss_trn(2:end,:) = clip(X_trn * X_trn');
+        Kss_val(2:end,:) = clip(X_trn * X_val'); Rss(:,3) = toc;
+        Kss_tst(2:end,:) = clip(X_trn * X_tst');
+        
+        Kts_trn = Kss_trn;
+        Kts_val = Kss_val;
+        Kts_tst = Kss_tst;
+        Rts     = Rss;
+        
+        for l = 0:L
+            for k = [2 1]
                 
-                fprintf('%s: c=%.1e, l=%02d, ', name{k}, c, l);
+                if k == 1, fprintf('%s: c=%d, l=%02d, ', 'SSReLU', c, l);
+                else,      fprintf('%s: c=%d, l=%02d, ', 'TSReLU', c, l); end
                 
-                tic;
-                model = svmtrain_inplace(y_trn, K_trn, sprintf('-t 4 -c %d -q', c));
-                fprintf('nSV=%d, ', model.totalSV);
+                if k == 1, [Rss(l+1,1), Rss(l+1,2), t] = svm(Kss_trn, y_trn, Kss_val, y_val, Kss_tst, y_tst, c);
+                else,      [Rts(l+1,1), Rts(l+1,2), t] = svm(Kts_trn, y_trn, Kts_val, y_val, Kts_tst, y_tst, c); end
                 
-                [~, tmp, ~]    = svmpredict_inplace(y_val, K_val, model); t = toc;
-                fprintf('\b, '); svmpredict_inplace(y_tst, K_tst, model);
-                fprintf('\b, t=%.2f\n', t_base+t);
+                if k == 1, Rss(l+1,3) = Rss(l+1,3) + t;
+                else,      Rts(l+1,3) = Rts(l+1,3) + t; end
                 
-                if tmp(1) >= acc, acc = tmp(1); else, break; end
+                if l == L, continue; end
                 
-                tic;
-                K_trn(2:end,:) = clip(kern{k}(K_trn(2:end,:)));
-                K_val(2:end,:) = clip(kern{k}(K_val(2:end,:))); t_base = t_base + toc;
-                K_tst(2:end,:) = clip(kern{k}(K_tst(2:end,:)));
+                if k == 1
+                    tic;
+                    Kss_trn(2:end,:) = clip(ssrelu(Kss_trn(2:end,:)));
+                    Kss_val(2:end,:) = clip(ssrelu(Kss_val(2:end,:))); t = toc;
+                    Kss_tst(2:end,:) = clip(ssrelu(Kss_tst(2:end,:)));
+                    
+                    Rss((l+2):end,3) = Rss((l+2):end,3) + t;
+                    Rts((l+2):end,3) = Rts((l+2):end,3) + t;
+                else
+                    tic;
+                    Kts_trn(2:end,:) = clip(tsrelu(Kss_trn(2:end,:),Kts_trn(2:end,:)));
+                    Kts_val(2:end,:) = clip(tsrelu(Kss_val(2:end,:),Kts_val(2:end,:))); t = toc;
+                    Kts_tst(2:end,:) = clip(tsrelu(Kss_tst(2:end,:),Kts_tst(2:end,:)));
+                    
+                    Rts((l+2):end,3) = Rts((l+2):end,3) + t;
+                end
             end
         end
+        
+        save([datasets{i} '-' sprintf('%d', c) '.mat'], 'Rss', 'Rts');
     end
 end
 
 diary off;
+
+end
+
+function [val_acc, tst_acc, t] = svm(K_trn, y_trn, K_val, y_val, K_tst, y_tst, c)
+
+tic;
+model = svmtrain_inplace(y_trn, K_trn, sprintf('-t 4 -c %d -q', 10^c));
+fprintf('nSV=%d, ', model.totalSV);
+
+[~, val_acc, ~] = svmpredict_inplace(y_val, K_val, model); val_acc = val_acc(1);
+t = toc; fprintf('\b, ');
+[~, tst_acc, ~] = svmpredict_inplace(y_tst, K_tst, model); tst_acc = tst_acc(1);
+
+end
---
+++ experiments/table/rep_mlp.m
@@ -0,0 +1,50 @@
+D = {'mnist' 'cifar10' 'svhn2'};
+M = {'0' '1' '2' '3'};
+
+for d = D
+    for m = M
+
+        v1 = 0;  va = 0;
+        t1 = 0;  ta = 0;
+        n1 = ''; na = '';
+        s1 = 0;  sa = 0;
+        s = [];
+        
+        F = strsplit(ls(['mlp/*/' d{1} '-' m{1} '*.mat']));
+        
+        for f = F
+            
+            if isempty(f{1}), continue; end
+            
+            load(f{1});
+            
+            if val_acc(1) > v1
+                v1 = val_acc(1);
+                t1 = tst_acc(1);
+                n1 = f{1};
+                s1 = mean(time);
+            end
+            
+            [bv, bi] = max(val_acc);
+            if bv > va
+                va = bv;
+                ta = tst_acc(bi);
+                na = f{1};
+                sa = mean(time);
+            end
+            
+            s = [s time];
+        end
+        
+        s = mean(s);
+        
+        if m{1} == '0'
+            sn  = s;
+            s1n = s1;
+            san = sa;
+        end
+        
+        fprintf('%s m-%s e1 %s %.2f %.1f     \n', d{1}, m{1}, n1, (1-t1)*100, s1/s1n);
+        fprintf('%s m-%s ef %s %.2f %.1f %.1f\n', d{1}, m{1}, na, (1-ta)*100, sa/san, s/sn);
+    end
+end
